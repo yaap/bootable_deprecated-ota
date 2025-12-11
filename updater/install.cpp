1cbf6188c1acb4c77d1cfa9eb5197c65f2f02bae
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 11:45:38 +0200

    updater: Fix unused-but-set-variable errors
    Change-Id: Iaae85c4d73f614216105d38abd8d52efea892ed6

--- updater/install.cpp
+++ updater/install.cpp
@@ -677,7 +677,7 @@ Value* SymlinkFn(const char* name, State* state, const std::vector<std::unique_p
     return ErrorAbort(state, kArgsParsingFailure, "%s() target argument can't be empty", name);
   }
 
-  size_t bad = 0;
+  [[maybe_unused]] size_t bad = 0;
   for (size_t i = 1; i < args.size(); ++i) {
     const auto& src = args[i];
     if (unlink(src.c_str()) == -1 && errno != ENOENT) {
